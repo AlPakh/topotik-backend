613c620bda2aa1cc5d9efb3a603a09d74a1264b8
AlPakh <alvp2003@yandex.com>
2025-05-01 01:23:27 +0300

    base route greets word now

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import init_db
 from app.routers import auth, users, maps, collections, markers
 
-app = FastAPI(title="Topotik API")
-
 # CORS
 app.add_middleware(
     CORSMiddleware,
@@ -17,6 +15,10 @@ app.add_middleware(
 @app.on_event("startup")
 def on_startup():
     init_db()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(users.router, prefix="/users")
